40246e9f36488cbbc6232bb3db311a6f30f240f6
Alumno <Alumno@LABSI-13>
2015-10-13 11:03:21 -0500

    Ejercicio 3

---
+++ Ejercicio3/src/ejercicio3.py
@@ -0,0 +1,11 @@
+
+
+#Contabilidad de las vacunas de la ONG
+
+a=1000
+if a>=200:
+    while a>=200:
+        i=input('Ingrese la cantidad de vacunas que se desea repartir')
+        a=a-i
+    else:
+        print 'Tenga cuidado la canitdad de vacunas restantes ya es menor a 200'
